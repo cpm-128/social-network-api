4fe6c465490119d1722e34b9ac925c40fb60bd37
cpm-128 <101371032+cpm-128@users.noreply.github.com>
2022-08-15 12:38:59 -0400

    routes for CRUDing thoughts and reactions

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,12 @@
 // import all the API routes to prefix their endpoint names and package them all up
 
 const router = require('express').Router();
+const thoughtRoutes = require('./thought-routes');
 const userRoutes = require('./user-routes');
 
-// add prefixe of /users to routes created in user-routes.js
+// add prefix of /thoughts to routes created in thought-routes.js
+router.use('/thoughts', thoughtRoutes);
+// add prefix of /users to routes created in user-routes.js
 router.use('/users', userRoutes);
 
 module.exports = router;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+// import the controller methods
+const {
+    addThought,
+    getAllThought,
+    getSingleThought,
+    updateThought,
+    removeThought,
+    addReaction,
+    removeReaction
+} = require('../../controllers/thought-controller');
+
+// set up GET all and POST at /api/thoughts
+router
+    .route('/')
+    .get(getAllThought)
+    .post(addThought);
+
+// set up GET single, PUT, and DELETE at api/thoughts/:id
+router
+    .route('/:id')
+    .get(getSingleThought)
+    .put(updateThought)
+    .delete(removeThought);
+
+// set up POST and DELETE for reactions at /api/thoughts/:thoughtId/reactions
+router
+    .route(':/thoughtId/reactions')
+    .post(addReaction)
+    .delete(removeReaction);
+
+module.exports = router;
